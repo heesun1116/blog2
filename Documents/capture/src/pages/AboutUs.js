80fec878be6548e78a850cd07d8ea018dce46a57
Heesun <heeson0924@gmail.com>
2021-02-15 17:17:32 +0900

    about section

---
+++ Documents/capture/src/pages/AboutUs.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import AboutSection from '../components/AboutSection';
+//page Components
+
+
+const AboutUs = () => {
+  return (
+    <div>
+      <AboutSection />
+      
+    </div>
+  );
+};
+
+export default AboutUs;
